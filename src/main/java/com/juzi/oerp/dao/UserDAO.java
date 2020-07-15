32f502658db5e425e7196fd7c126f87babc87719
桔子 <juzi214032@qq.com>
2020-07-15 23:12:12 +0800

    style: 完善 UserDAO 注释

--- src/main/java/com/juzi/oerp/dao/UserDAO.java
+++ src/main/java/com/juzi/oerp/dao/UserDAO.java
@@ -10,7 +10,17 @@ import org.apache.ibatis.annotations.Param;
  * @date 2020/7/15 14:08
  */
 public interface UserDAO {
+    /**
+     * 获取用户_分页
+     * @param page 分页参数
+     * @return 用户信息
+     */
     IPage<UserInfoVO> getUserByPage(Page<UserInfoVO> page);
 
+    /**
+     * 获取用户_根据用户 id
+     * @param userId 用户 id
+     * @return 用户信息
+     */
     UserInfoVO getUserByUserId(@Param("userId") Integer userId);
 }
