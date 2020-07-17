e6a77f3e4167528fea1bb62082cd490da6b118b0
桔子 <juzi214032@qq.com>
2020-07-17 10:41:21 +0800

    feat: 完善考生报名逻辑

--- src/main/java/com/juzi/oerp/controller/user/ApplyController.java
+++ src/main/java/com/juzi/oerp/controller/user/ApplyController.java
@@ -1,7 +1,10 @@
 package com.juzi.oerp.controller.user;
 
 import com.juzi.oerp.model.dto.ApplyExamParamDTO;
+import com.juzi.oerp.model.vo.response.CreateResponseVO;
 import com.juzi.oerp.model.vo.response.ResponseVO;
+import com.juzi.oerp.service.ApplyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/apply")
 public class ApplyController {
+    
+    @Autowired
+    private ApplyService applyService;
+
     /**
      * 用户报名考试
      * @param applyExamParamDTO 报名参数
@@ -23,7 +30,8 @@ public class ApplyController {
      */
     @PostMapping
     public ResponseVO<Object> applyExam(@RequestBody ApplyExamParamDTO applyExamParamDTO){
-        return null;
+        applyService.applyExam(applyExamParamDTO);
+        return new CreateResponseVO();
     }
 
 }
