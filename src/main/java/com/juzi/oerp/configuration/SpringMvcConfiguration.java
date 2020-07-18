c34daed1234c23c4f8bd462b49ce0c658cc64bfc
桔子 <juzi214032@qq.com>
2020-07-18 18:35:26 +0800

    feat: 增加跨域配置

--- src/main/java/com/juzi/oerp/configuration/SpringMvcConfiguration.java
+++ src/main/java/com/juzi/oerp/configuration/SpringMvcConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
@@ -33,6 +34,17 @@ public class SpringMvcConfiguration extends DelegatingWebMvcConfiguration {
 
     @Autowired
     private LocalDateTimeDeserializer localDateTimeDeserializer;
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .maxAge(3000)
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET","POST","PUT","DELETE");
+        super.addCorsMappings(registry);
+    }
 
     /**
      * 配置拦截器
