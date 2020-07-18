3c9a2c32c410625c8da6a7ac056f547b90aec9de
桔子 <juzi214032@qq.com>
2020-07-18 09:51:22 +0800

    refactor: 去除 ApplyController Bean 的命名

--- src/main/java/com/juzi/oerp/controller/user/ApplyController.java
+++ src/main/java/com/juzi/oerp/controller/user/ApplyController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Juzi
  * @date 2020/7/15 23:03
  */
-@RestController("userApplyController")
+@RestController
 @RequestMapping("/user/apply")
 public class ApplyController {
 
