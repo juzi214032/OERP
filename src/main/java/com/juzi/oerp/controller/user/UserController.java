8a409523e03988c9b82d146d4a85a65ec2c24d94
桔子 <juzi214032@qq.com>
2020-07-19 20:55:37 +0800

    feat: 增加获取个人信息接口

---
+++ src/main/java/com/juzi/oerp/controller/user/UserController.java
@@ -0,0 +1,35 @@
+package com.juzi.oerp.controller.user;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.juzi.oerp.common.store.LocalUserStore;
+import com.juzi.oerp.model.po.UserInfoPO;
+import com.juzi.oerp.model.vo.response.ResponseVO;
+import com.juzi.oerp.service.UserInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Juzi
+ * @date 2020/7/19 20:48
+ */
+@RestController
+@RequestMapping("/user")
+@Api(tags = "用户信息")
+public class UserController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @GetMapping
+    @ApiOperation(value = "个人信息", notes = "获取当前已登录用户的个人信息")
+    public ResponseVO<UserInfoPO> getUserInfo() {
+        Integer userId = LocalUserStore.getLocalUser();
+        UserInfoPO userInfo = userInfoService.getOne(new LambdaQueryWrapper<UserInfoPO>().eq(UserInfoPO::getUserId, userId));
+        return new ResponseVO<>(userInfo);
+    }
+
+}
