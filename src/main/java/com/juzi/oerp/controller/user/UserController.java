97f566449634d0c19d265f15abc6090548d5ee37
桔子 <juzi214032@qq.com>
2020-07-20 14:37:35 +0800

    feat: 新增修改用户信息接口

--- src/main/java/com/juzi/oerp/controller/user/UserController.java
+++ src/main/java/com/juzi/oerp/controller/user/UserController.java
@@ -2,13 +2,18 @@ package com.juzi.oerp.controller.user;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.juzi.oerp.common.store.LocalUserStore;
+import com.juzi.oerp.model.dto.UpdateUserInfoDTO;
 import com.juzi.oerp.model.po.UserInfoPO;
 import com.juzi.oerp.model.vo.response.ResponseVO;
+import com.juzi.oerp.model.vo.response.UpdatedResponseVO;
 import com.juzi.oerp.service.UserInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Juzi
  * @date 2020/7/19 20:48
  */
-@RestController("userUserController")
-@RequestMapping("/user")
 @Api(tags = "用户信息")
+@RequestMapping("/user")
+@RestController("userUserController")
 public class UserController {
 
     @Autowired
@@ -30,6 +35,18 @@ public class UserController {
         Integer userId = LocalUserStore.getLocalUser();
         UserInfoPO userInfo = userInfoService.getOne(new LambdaQueryWrapper<UserInfoPO>().eq(UserInfoPO::getUserId, userId));
         return new ResponseVO<>(userInfo);
+    }
+
+    @PutMapping
+    @ApiOperation(value = "修改个人信息")
+    public UpdatedResponseVO updateUserInfo(@RequestBody UpdateUserInfoDTO updateUserInfoDTO) {
+        Integer userId = LocalUserStore.getLocalUser();
+        UserInfoPO userInfoPO = new UserInfoPO();
+        userInfoPO.setUserId(userId);
+        BeanUtils.copyProperties(updateUserInfoDTO, userInfoPO);
+
+        userInfoService.updateById(userInfoPO);
+        return new UpdatedResponseVO();
     }
 
 }
---
+++ src/main/java/com/juzi/oerp/model/dto/UpdateUserInfoDTO.java
@@ -0,0 +1,29 @@
+package com.juzi.oerp.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 更新用户信息
+ *
+ * @author Juzi
+ * @date 2020/7/20 14:32
+ */
+@Data
+@ApiModel(description = "修改用户信息参数")
+public class UpdateUserInfoDTO {
+
+    @ApiModelProperty("姓名")
+    private String name;
+
+    @ApiModelProperty("身份证号")
+    private String identityNo;
+
+    @ApiModelProperty("性别")
+    private Integer gender;
+
+    @ApiModelProperty("学历")
+    private String education;
+
+}
