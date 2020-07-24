6c72024e91bfee6e3545432cd2b40a21376c45d4
桔子 <juzi214032@qq.com>
2020-07-24 15:17:03 +0800

    fix: 删除短信登录时的短信验证码参数

--- src/main/java/com/juzi/oerp/model/dto/UserSMSLoginDTO.java
+++ src/main/java/com/juzi/oerp/model/dto/UserSMSLoginDTO.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 /**
  * @author Juzi
@@ -18,8 +17,4 @@ public class UserSMSLoginDTO {
     @ApiModelProperty("手机号")
     @Pattern(regexp = "(?:(?:\\+|00)86)?1(?:(?:3[\\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\\d])|(?:9[1|8|9]))\\d{8}", message = "手机号格式错误")
     private String phoneNumber;
-
-    @Size(min = 6, max = 6,message = "验证码长度应为6位数字")
-    @ApiModelProperty("验证码")
-    private String captcha;
 }
