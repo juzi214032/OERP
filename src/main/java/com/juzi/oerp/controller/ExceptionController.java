dcfc12b7f04c11b2efec43d3a65e09785570cda6
桔子 <juzi214032@qq.com>
2020-07-30 17:17:59 +0800

    refactor: 修改异常处理信息

--- src/main/java/com/juzi/oerp/controller/ExceptionController.java
+++ src/main/java/com/juzi/oerp/controller/ExceptionController.java
@@ -58,7 +58,7 @@ public class ExceptionController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ExceptionResponseVO methodArgumentNotValidException(MethodArgumentNotValidException e) {
         String validMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        return new ExceptionResponseVO(40000, validMessage);
+        return new ExceptionResponseVO(40000, "参数格式错误", validMessage);
     }
 
     /**
